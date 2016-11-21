a0167ee8a1d56dc10d63d14f4a166f730ebca59a
Derek Anderson <derek@hvst.com>
2016-11-21 17:08:50 -0600

    stub

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+def long_description():
+  os.system('pandoc --from=markdown --to=rst --output=README.rst README.md')
+  readme_fn = os.path.join(os.path.dirname(__file__), 'README.rst')
+  if os.path.exists(readme_fn):
+    with open(readme_fn) as f:
+      return f.read()
+  else:
+    return 'not available'
+
+setup(
+  name='bottle-react',
+  version=__import__('bottlereact').__version__,
+  description='A microframework for Bottle+React projects.',
+  long_description=long_description(),
+  author='Derek Anderson',
+  author_email='public@kered.org',
+  url='https://github.com/keredson/bottle-react',
+  packages=[],
+  py_modules=['bottlereact'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+  ],
+  install_requires=[],
+)
+
